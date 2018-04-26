c3af41c9260383cb3ad4fe507703454b8d94bc98
kindofwhat <kindofwhat@gmail.com>
2018-04-26 08:25:39 +0200

    bugfix

--- src/main/kotlin/facts/implementations.kt
+++ src/main/kotlin/facts/implementations.kt
@@ -3,9 +3,9 @@ package facts
 /**
  * returns first max value with a confidence of 1.0
  */
-class MaxEvaluator<T,V:Comparable<V>>:Evaluator<T,V> {
+class MaxEvaluator<U:Comparable<U>>:Evaluator<U> {
     val name="max-evaluator"
-    override fun evaluatePropositions(propositions: Set<Proposition<T, V>>): Set<PropositionEvaluation<T, V>> {
+    override fun evaluatePropositions(propositions: Set<Proposition<U>>): Set<PropositionEvaluation<U>> {
         val max = propositions
                 .maxBy { proposition -> proposition.proposal }
         if(max != null) return setOf(PropositionEvaluation(max,1.0f,name))
