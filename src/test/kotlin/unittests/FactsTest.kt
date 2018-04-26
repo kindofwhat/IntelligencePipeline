caccef5a91dcb5f8df5db7f1257e0700e807252d
kindofwhat <kindofwhat@gmail.com>
2018-04-26 08:34:05 +0200

    bugfix

--- src/test/kotlin/unittests/FactsTest.kt
+++ src/test/kotlin/unittests/FactsTest.kt
@@ -6,24 +6,17 @@ import facts.PropositionEvaluation
 import org.junit.Test
 
 class FactsTest {
-    data class TestProposition(override val proposedFor: String, override val proposal: Int = proposedFor.length, override val confidence: Float):
-            Proposition<String,Int>,Comparable<Proposition<String,Int>> {
-        override fun compareTo(other: Proposition<String, Int>): Int {
-            return proposedFor.compareTo(other.proposedFor)
-        }
-    }
+    class TestProposition(proposal: Int, confidence: Float) : Proposition<Int>(proposal, confidence)
 
     @Test
     fun testMaxEvaluator() {
 
-        val evaluator = MaxEvaluator<String,Int>()
+        val evaluator = MaxEvaluator<Int>()
 
-        val res = evaluator.evaluatePropositions(sortedSetOf(
-                TestProposition(proposedFor = "aaa", confidence = 0.6f),
-                TestProposition(proposedFor = "two", confidence = 0.1f),
-                TestProposition(proposedFor = "three", confidence = 0.6f)
-
-                ))
+        val res = evaluator.evaluatePropositions(setOf(
+                TestProposition(12, confidence = 0.6f),
+                TestProposition(1, confidence = 0.1f),
+                TestProposition(14, confidence = 0.6f)))
         assert(res.size==1)
     }
 
